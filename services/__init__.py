1305531b50821430124190ffb863150051103b75
Niko23 <50606486+NiiKo23@users.noreply.github.com>
2026-05-17 15:20:47 -0300

    Add services __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,17 @@
+"""
+AutomataLol - Services Init
+"""
+
+from .lcu_service import lcu_service, get_lcu_service, LCUService, LCUServiceState
+from .config_service import config_service, get_config_service, ConfigService, AppConfig
+
+__all__ = [
+    "lcu_service",
+    "get_lcu_service",
+    "LCUService",
+    "LCUServiceState",
+    "config_service",
+    "get_config_service",
+    "ConfigService",
+    "AppConfig",
+]
